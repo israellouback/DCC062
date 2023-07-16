02ec1551cb37aa130c5fe6dd5c1901c0068f316f
Israel Louback <israel.louback@engenharia.ufjf.br>
2023-07-16 20:23:22 -0300

    .

--- Process/src/main/java/com/mycompany/process/Escalonador.java
+++ Process/src/main/java/com/mycompany/process/Escalonador.java
@@ -5,40 +5,49 @@ import java.util.List;
 public class Escalonador  {
     
     //Atributos
-   private List<Process> listProcess;
+   private static  List<Process> listProcess;
 
     public Escalonador(List<Process> processos ) {
         this.listProcess = processos;
     }
    
-   
    //Métodos
    public void addProcess(List<Process> listProcess,Process p1){
        listProcess.add(p1);
-   }
-   public void escalonadorAdmissao(List<Process> listProcess)
-   {
        
    }
-   
-   public void escalonadorMemoria(List<Process> listProcess)
+   public List<Process> escalonadorAdmissao(List<Process> listProcess)
    {
+     
+       for(int i=0;i<listProcess.size();i++){
+           for(int j=i;j<listProcess.size();j++){
+         
+            }
+            }
        
        
-   }
-   
-   public void escalonadorCPU(List<Process> listProcess)
-   {
-       
+      return listProcess;
    }
    
-   public void escalonador(List<Process> listProcess)
+   public List<Process>  escalonadorMemoria(List<Process> listProcess)
    {
        
+         return listProcess;
+   }
+   
+   public List<Process>  escalonadorCPU(List<Process> listProcess)
+   {
+       
+    return listProcess;
+   }
+   
+   public List<Process>  escalonador(List<Process> listProcess)
+   {
+       
+        return listProcess;
    }
    public static void main(String[] args)
    {
-  
     Process p1= new Process("p1",2,3,15);
    // Process p2= new Process("p2",3,8,125);
    // Process p3= new Process("p3",4,7,35);
@@ -47,7 +56,6 @@ public class Escalonador  {
    // Process p6= new Process("p6",9,4,85);
     //Process p7= new Process("p7",11,5,95);
     //e1.add(p1);
-  
    }
    
     
